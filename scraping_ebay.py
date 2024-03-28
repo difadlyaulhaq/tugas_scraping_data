5f3728f65a5a5a371b91a702cb804c5b6dd3320e
difadlyaulhaq <difadlyaulhaq2@gmail.com>
2024-03-28 11:33:22 +0700

    update

--- scraping_ebay.py
+++ scraping_ebay.py
@@ -6,23 +6,48 @@ import time
 
 option = webdriver.ChromeOptions()
 option.add_argument("--headless")
-servis = Service('chromedriver.exe')
+service = Service('chromedriver.exe')
 
-driver = webdriver.Chrome(service=servis,options=option)
+driver = webdriver.Chrome(service=service, options=option)
 
-url = "https://www.ebay.com/e/row/pre-loved-watches"
-driver.set_window_size(1300,800)
-driver.get(url)
-time.sleep(5)
-driver.save_screenshot("ebay_watches.png")
-content = driver.page_source
+base_url = "https://www.ebay.com/e/row/pre-loved-watches?_pgn={}"
+
+jumlah_halaman = 10
+list_nama, list_gambar, list_harga, list_link, list_dilihat = [], [], [], [], []
+
+for halaman in range(1, jumlah_halaman + 1):
+    url = base_url.format(halaman)
+    driver.get(url)
+    time.sleep(3)  
+
+    content = driver.page_source
+    data = BeautifulSoup(content, 'html.parser')
+
+    # Memeriksa apakah halaman berhasil dimuat
+    if "pre-loved-watches" not in driver.current_url:
+        print(f"Gagal memuat halaman {halaman}. Melanjutkan ke halaman berikutnya...")
+        continue
+
+    print(f"Scraping data dari halaman {halaman}")
+
+    for area in data.find_all('div', class_="s-item__wrapper clearfix"):
+        nama = area.find('h3', class_="s-item__title").get_text()
+        gambar = area.find('img')['src']
+        harga = area.find('span', class_="s-item__price").get_text()
+        link = "https://ebay.com" + area.find('a')['href']
+        dilihat = area.find('div', class_="NEGATIVE")
+        if dilihat != None:
+            dilihat = dilihat.get_text()
+
+        list_nama.append(nama)
+        list_gambar.append(gambar)
+        list_harga.append(harga)
+        list_link.append(link)
+        list_dilihat.append(dilihat)
+
+df = pd.DataFrame({'Nama': list_nama, 'Gambar': list_gambar, 'Harga': list_harga, 'Link': list_link, 'Dilihat': list_dilihat})
+df.to_csv('watch3.csv', index=False)
+
+
+
 driver.quit()
-
-data = BeautifulSoup(content,'html.parser')
-print(data.encode("utf-8"))
-i = 1
-for area in data.find_all('div', class_="s-item__wrapper clearfix"):
-    print(i)
-    print(area)
-    i += 1
-    print("-------------")
